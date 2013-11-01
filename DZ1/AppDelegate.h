8b0480c1c65f650a825f19625ac0b2e4a7c3810d
Anton Batuev <antbatuev@mail.ru>
2013-11-01 21:05:50 +0400

    Initial Commit

---
+++ DZ1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DZ1
+//
+//  Created by Admin on 11/1/13.
+//  Copyright (c) 2013 MSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
